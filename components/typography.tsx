ac35cdfa04a1cd6e5b215c1eb898a230263af7df
realfr <120209053+realfr@users.noreply.github.com>
2023-01-19 19:51:10 -0700

    fix: typography sizing

--- components/typography.tsx
+++ components/typography.tsx
@@ -2,6 +2,21 @@ import styled from "styled-components";
 
 export const D1 = styled.h1`
   font-family: Zephyr;
+  font-size: 5.61rem;
+  line-height: 120%;
+  letter-spacing: 0.03em;
+  overflow-wrap: break-word;
+  color: ${(p) => p.theme.colors.green};
+  @media screen and (min-width: 568px) {
+    font-size: calc(7.478rem + 16 * ((100vw - 568px) / (768-568)));
+  }
+  @media screen and (min-width: 768px) {
+    font-size: 9.969rem;
+  }
+`;
+
+export const D2 = styled.h2`
+  font-family: Zephyr;
   font-size: 4.209rem;
   line-height: 120%;
   letter-spacing: 0.03em;
@@ -15,7 +30,7 @@ export const D1 = styled.h1`
   }
 `;
 
-export const D2 = styled.h2`
+export const H1 = styled.h1`
   font-family: Zephyr;
   font-size: 3.157rem;
   line-height: 120%;
@@ -30,7 +45,7 @@ export const D2 = styled.h2`
   }
 `;
 
-export const H1 = styled.h1`
+export const H2 = styled.h2`
   font-family: Zephyr;
   font-size: 2.369rem;
   line-height: 120%;
@@ -44,8 +59,7 @@ export const H1 = styled.h1`
     font-size: 4.209rem;
   }
 `;
-
-export const H2 = styled.h2`
+export const H3 = styled.h3`
   font-family: Zephyr;
   font-size: 1.777rem;
   line-height: 120%;
@@ -59,7 +73,7 @@ export const H2 = styled.h2`
     font-size: 3.157rem;
   }
 `;
-export const H3 = styled.h3`
+export const H4 = styled.h4`
   font-family: Zephyr;
   font-size: 1.333rem;
   line-height: 120%;
@@ -73,12 +87,13 @@ export const H3 = styled.h3`
     font-size: 2.369rem;
   }
 `;
-export const H4 = styled.h4`
+export const H5 = styled.h5`
   font-family: Zephyr;
   font-size: 1rem;
   line-height: 120%;
   letter-spacing: 0.03em;
   overflow-wrap: break-word;
+  text-transform: uppercase;
   color: ${(p) => p.theme.colors.green};
   @media screen and (min-width: 568px) {
     font-size: calc(1.333rem + 16 * ((100vw - 568px) / (768-568)));
@@ -87,7 +102,7 @@ export const H4 = styled.h4`
     font-size: 1.777rem;
   }
 `;
-export const H5 = styled.h5`
+export const H6 = styled.h6`
   font-family: Zephyr;
   font-size: 0.75rem;
   line-height: 120%;
@@ -102,30 +117,15 @@ export const H5 = styled.h5`
     font-size: 1.333rem;
   }
 `;
-export const H6 = styled.h6`
-  font-family: Zephyr;
-  font-size: 0.563rem;
-  line-height: 120%;
-  letter-spacing: 0.03em;
-  overflow-wrap: break-word;
-  text-transform: uppercase;
-  color: ${(p) => p.theme.colors.green};
-  @media screen and (min-width: 568px) {
-    font-size: calc(0.75rem + 16 * ((100vw - 568px) / (768-568)));
-  }
-  @media screen and (min-width: 768px) {
-    font-size: 1rem;
-  }
-`;
 
 export const B = styled.span`
   font-family: Satoshi;
   font-weight: 500;
-  font-size: 2.369rem;
+  font-size: 0.75rem;
   line-height: 120%;
   letter-spacing: 0.05em;
   @media screen and (min-width: 568px) {
-    font-size: calc(1.777rem + 16 * ((100vw - 568px) / (768-568)));
+    font-size: calc(1rem + 16 * ((100vw - 568px) / (768-568)));
   }
   @media screen and (min-width: 768px) {
     font-size: 1.333rem;
@@ -134,53 +134,53 @@ export const B = styled.span`
 export const P1 = styled.p`
   font-family: Satoshi;
   font-weight: 400;
-  font-size: 1.777rem;
-  line-height: 120%;
-  letter-spacing: 0.05em;
-  @media screen and (min-width: 568px) {
-    font-size: calc(1.333rem + 16 * ((100vw - 568px) / (768-568)));
-  }
-  @media screen and (min-width: 768px) {
-    font-size: 1rem;
-  }
-`;
-export const P2 = styled.p`
-  font-family: Satoshi;
-  font-weight: 400;
-  font-size: 1.333rem;
+  font-size: 0.75rem;
   line-height: 120%;
   letter-spacing: 0.05em;
   @media screen and (min-width: 568px) {
     font-size: calc(1rem + 16 * ((100vw - 568px) / (768-568)));
   }
   @media screen and (min-width: 768px) {
-    font-size: 0.75rem;
+    font-size: 1.333rem;
   }
 `;
-export const C = styled.caption`
+export const P2 = styled.p`
   font-family: Satoshi;
   font-weight: 400;
-  font-size: 1rem;
+  font-size: 0.563rem;
   line-height: 120%;
   letter-spacing: 0.05em;
   @media screen and (min-width: 568px) {
     font-size: calc(0.75rem + 16 * ((100vw - 568px) / (768-568)));
   }
   @media screen and (min-width: 768px) {
-    font-size: 0.563rem;
+    font-size: 1rem;
   }
 `;
-export const L = styled.label`
+export const C = styled.caption`
   font-family: Satoshi;
   font-weight: 400;
-  font-size: 0.75rem;
+  font-size: 0.422rem;
   line-height: 120%;
   letter-spacing: 0.05em;
   @media screen and (min-width: 568px) {
     font-size: calc(0.563rem + 16 * ((100vw - 568px) / (768-568)));
   }
   @media screen and (min-width: 768px) {
-    font-size: 0.422rem;
+    font-size: 0.75rem;
+  }
+`;
+export const L = styled.label`
+  font-family: Satoshi;
+  font-weight: 400;
+  font-size: 0.422rem;
+  line-height: 120%;
+  letter-spacing: 0.05em;
+  @media screen and (min-width: 568px) {
+    font-size: calc(0.422rem + 16 * ((100vw - 568px) / (768-568)));
+  }
+  @media screen and (min-width: 768px) {
+    font-size: 0.563rem;
   }
 `;
 
